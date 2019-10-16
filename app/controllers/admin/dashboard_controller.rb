add50f7371681ded47d29cf0966225c17970f678
Eddie <eddietseng@outlook.com>
2019-10-16 19:41:28 -0400

    Added information to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
 
   def show
-    
+    @products = Product.order("id")
+    @categories = Category.order("id")
   end
 end
